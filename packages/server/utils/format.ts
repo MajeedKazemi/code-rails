885256e7617ec9c827285f43c7f43f658e3fd52c
Chase McDougall <chasemcdougall@hotmail.com>
2023-10-13 18:11:05 -0400

    Setup code formatter

--- packages/server/utils/format.ts
+++ packages/server/utils/format.ts
@@ -1,5 +1,32 @@
+import { spawn } from "child_process";
+
 export const removeComments = (code: string) =>
     code
         .replace(/\s*?#.*?\n/g, "") // single line comments
         .replace(/'''[\s\S]*?'''/g, "") // multi-line comments
         .replace(/"""[\s\S]*?"""/g, ""); // multi-line comments
+
+export const formatPythonCode = (code: string): Promise<string> =>
+    new Promise((resolve, reject) => {
+        const blackFormat = spawn("black", [
+            "--code", code,
+        ]);
+        
+        let formattedCode = "";
+        
+        blackFormat.stdout.on("data", (data) => {
+            formattedCode += data.toString();
+        });
+        
+        blackFormat.stderr.on("data", (data) => {
+            reject(data.toString());
+        });
+        
+        blackFormat.on("close", (code: number) => {
+            if (code === 0) {
+                resolve(formattedCode);
+            } else {
+                reject(`clang-format exited with code ${code}`);
+            }
+        });
+    });
