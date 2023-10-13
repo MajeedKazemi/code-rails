39602890b0a1180e53c50aec63e44eda7c439fc6
Chase McDougall <chasemcdougall@hotmail.com>
2023-10-13 18:24:42 -0400

    Create parsers.ts

---
+++ packages/server/utils/parsers.ts
@@ -0,0 +1,64 @@
+export const rawFixedCodeParser = (r: string) => {
+    const obj: any = {
+        rawFixedCode: r,
+    };
+
+    // remove [end-fixed-code] from the end of the file
+
+    if (r.endsWith("\n[end-fixed-code]")) {
+        obj.rawFixedCode = r.replace("\n[end-fixed-code]", "");
+    }
+
+    return obj;
+};
+
+export const diffFixedCodeParser = (txt: string) => {
+    const r = `[explained-original-code]:\n${txt}`;
+
+    const obj: any = {
+        rawExplainedLines: "",
+        explanation: "",
+    };
+
+    const lines = r.split("\n");
+
+    let inLineByLineExplanation = false;
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+
+        if (line.startsWith("[explained-original-code]")) {
+            inLineByLineExplanation = true;
+            continue;
+        } else if (line.startsWith("[end-explained-original-code]")) {
+            inLineByLineExplanation = false;
+            continue;
+        } else if (inLineByLineExplanation) {
+            obj.rawExplainedLines += line + "\n";
+        } else if (line.startsWith("[high-level-explanation-of-changes]")) {
+            obj.explanation = line.replace(
+                "[high-level-explanation-of-changes]: ",
+                ""
+            );
+        }
+    }
+
+    obj.rawExplainedLines = obj.rawExplainedLines.trim();
+
+    obj.lines = obj.rawExplainedLines.split("\n").map((line: string) => {
+        let splitter = " // [modified-reason]: ";
+
+        if (line.includes("// [added-reason]: ")) {
+            splitter = " // [added-reason]: ";
+        }
+
+        const [code, explanation] = line.split(splitter);
+
+        return {
+            code,
+            explanation,
+        };
+    });
+
+    return obj;
+};
