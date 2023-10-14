8c1932ae96cfbf2e6cdca1caa8a31935e8c98f43
Chase McDougall <chasemcdougall@hotmail.com>
2023-10-14 17:30:20 -0400

    remove MVP feedback generator

--- packages/server/routes/feedback-router.ts
+++ packages/server/routes/feedback-router.ts
@@ -81,36 +81,6 @@ feedbackRouter.post("/generate", verifyUser, async (req, res) => {
         feedback: explainedCode,
         success: true,
     });
-
-    if (false) { // (description !== undefined) {
-        const correctness_prompt = `Their solution is considered ${correctness ? "correct" : "incorrect"}.`
-        const prompt = `You are a helpful teaching assistant tasked with providing feedback on introductory Python coding problems.
-The teaching assistant will not provide code, but will provide feedback on the code that the student has written.
-The problem that the student is trying to solve is: ${description} with the correct solution being: ${solution}.
-The student has written the following code to solve the problem:\n${currentCode}\n${correctness_prompt}
-Please provide feedback on the code:\n`;
-
-        const result = await openai.chat.completions.create({
-            messages: [{ role: 'user', content: prompt }],
-            model: "gpt-3.5-turbo",
-            temperature: 0.5,
-            user: userId,
-        });
-
-        if (result.choices && result.choices?.length > 0) {
-            console.log("Returning response");
-            const feedback = result.choices[0].message.content;
-
-            res.json({
-                feedback: feedback,
-                success: true,
-            });
-        } else {
-            res.json({
-                success: false,
-            });
-        }
-    }
 });
 
 feedbackRouter.post("/correctness", verifyUser, async (req, res) => {
