457cab1c685c3c1c6f2393f672e527d5a81a0b72
Chase McDougall <chasemcdougall@hotmail.com>
2023-10-29 16:30:42 -0400

    removed nested paragraph

--- packages/client/src/components/feedback.tsx
+++ packages/client/src/components/feedback.tsx
@@ -78,14 +78,14 @@ export const Feedback = (props: FeedbackProps) => {
             <div className="border rounded-xl">
                 <div className="max-h-96 m-4">
                     <p><strong>Feedback:</strong></p>
-                    <p id="feedback-text">
+                    <div id="feedback-text">
                         <p className="flex flex-row gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="animate-spin w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                             </svg>
                             Generating Feedback...
                         </p>
-                    </p>
+                    </div>
                 </div>
             </div>
         )
