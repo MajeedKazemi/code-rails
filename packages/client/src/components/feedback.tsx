952d7d3080132d1ab3fe93f7e946cabc8d76d5ef
Chase McDougall <chasemcdougall@hotmail.com>
2023-10-29 14:31:08 -0400

    enable progressive feedback

--- packages/client/src/components/feedback.tsx
+++ packages/client/src/components/feedback.tsx
@@ -2,24 +2,42 @@ import { Tooltip } from "react-tooltip";
 
 interface FeedbackProps {
     feedback: any;
+    iteration: number;
 }
 
 export const Feedback = (props: FeedbackProps) => {
-    if (props.feedback.explanation) {
-        return(
-            // <div className="border rounded-xl max-h-96 overflow-y-auto p-4">
-            <div className="border rounded-xl bg-indigo-900">
-                <div className="rounded-t-xl bg-indigo-600 text-white p-2 flex gap-1">
+    const Heading = () => {
+        const headingClasses = "rounded-t-xl bg-indigo-600 text-white p-2 flex gap-1";
+        if (props.iteration > 1) {
+            return(
+                <div className={headingClasses}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59" />
                     </svg>
                     Hover over <p className="text-red-500">red</p> lines to see suggestions
                 </div>
-                <div className="p-2 font-mono max-h-96 overflow-y-auto whitespace-pre-wrap divide-y divide-indigo-900">
+            )
+        } else {
+            return(
+                <div className={headingClasses}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                    </svg>
+                    Feedback
+                </div>
+            )
+        }
+    };
+
+    const Body = () => {
+        const bodyClasses = "text-white p-2 max-h-96 overflow-y-auto whitespace-pre-wrap";
+        if (props.iteration > 1) {
+            return(
+                <div className={bodyClasses + " divide-y divide-indigo-900"}>
                     {props.feedback.lines.map((line: any, index: number) => {
                         return(
                             <div>
-                                <div id={`line_${index}`} className={`text-white min-h-[1.5rem] ${line.explanation ? "bg-red-500" : ""}`}>
+                                <div id={`line_${index}`} className={`min-h-[1.5rem] ${line.explanation ? "bg-red-500" : ""}`}>
                                     {line.code}
                                 </div>
                                 <Tooltip className="z-40 max-w-lg" anchorSelect={`#line_${index}`} place="right">
@@ -29,11 +47,22 @@ export const Feedback = (props: FeedbackProps) => {
                         )
                     })}
                 </div>
-            </div>
-                /* <div>
+            )
+        } else {
+            return(
+                <div className={bodyClasses}>
                     {props.feedback.explanation}
-                </div> */
-            // </div>
+                </div>
+            )
+        }
+    };
+
+    if (props.feedback.explanation) {
+        return(
+            <div className="border rounded-xl bg-indigo-900">
+                <Heading />
+                <Body />
+            </div>
         )
     } else {
         return(
