cf15d25f67f9007b74d644042090d688c09aa6a8
Chase McDougall <chasemcdougall@hotmail.com>
2023-11-19 16:37:14 -0500

    remove extra stuff

--- packages/server/prompts/level-three-feedback-prompt.ts
+++ packages/server/prompts/level-three-feedback-prompt.ts
@@ -25,14 +25,7 @@ Use the following template:
 - [line]: <number> [suggestion]: <hint and description>
 - [line]: <number> [suggestion]: <hint and description>
 - [line]: <number> [suggestion]: <hint and description>
-[[end-suggested-fixes]]You are a programming tutor. I am a novice student that is learning how to write Python code for the first time. I might have difficulties understanding the syntax and logic as well as many other basic computational thinking and meta cognitive skills.
-
-Look at the [intended-behavior] and [student-code] to first generate the [[numbered-fixed-student-code]]. The [[numbered-fixed-student-code]] should not go above and beyond to check every possible input. Just focus on making it work or achieving the [intended-behavior].
-
-Then, look back at the [student-code] and generate [[suggested-fixes]].
-Each line in [[suggested-fixes]] has two element:
-\`[line]: <number>\` where \`<number>\` corresponds to the line number in [numbered-fixed-student-code] that the [[suggested-fixes]] is associated with.
-[suggestion]: <hint and description> where \`<hint and description>\` is the details associated with the [[suggested-fixes]] for \`[line]\``,
+[[end-suggested-fixes]]`
         },
         {
             role: "user",
