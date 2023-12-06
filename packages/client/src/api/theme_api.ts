81e7573a61c9cf4aac0ba720fa066081c864b58b
Chase McDougall <chasemcdougall@hotmail.com>
2023-12-06 15:42:57 -0500

    Create theme_api.ts

---
+++ packages/client/src/api/theme_api.ts
@@ -0,0 +1,27 @@
+import env from "../utils/env";
+
+export const apiUpdateTheme = (token: string | null | undefined) =>
+    fetch(env.API_URL + "/api/theme/", {
+        method: "PUT",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+export const apiGetTheme = (
+        token: string | null | undefined,
+        theme: string
+    ) =>
+    fetch(env.API_URL + "/api/theme/", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            theme
+        })
+    });
