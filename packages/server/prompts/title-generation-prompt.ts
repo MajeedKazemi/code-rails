d5de3f3d8abbba5514e9cba9bc42e8e5913e5f28
Chase McDougall <chasemcdougall@hotmail.com>
2024-01-27 17:16:21 -0500

    initial title generation prompt

---
+++ packages/server/prompts/title-generation-prompt.ts
@@ -0,0 +1,49 @@
+import OpenAI from "openai";
+
+export const titleGenerationPrompt = (theme: string, task_description: string, code_solution: string) => {
+    const messages: Array<OpenAI.Chat.ChatCompletionMessageParam> = [
+        {
+            role: "system",
+            content: `using the provided [character] generate three [story-titles] as a way to incorporate storytelling (which includes a setup, conflict, and resolution) for the provided [python-programming-task].
+
+The story's conflict part should be about the provided [python-programming-task]
+
+Use the following template:
+[story-titles]:
+- <10-15 word story title>
+- <10-15 word story title>
+- <10-15 word story title>
+[end-story-titles]
+
+each story should have a completely different narrative and conflict. be imaginative. these will be provided to k-12 students to make the python tasks more engaging.`,
+        },
+        {
+            role: "user",
+            content: `[character]: ${theme}
+
+[python-programming-task]:
+${task_description}
+
+[code-solution]:
+${code_solution}`,
+        }
+    ];
+
+    return {
+        messages,
+        stop: [],
+        model: "gpt-4-1106-preview",
+        temperature: 0.5,
+        max_tokens: 1024,
+        parser: (resTxt: string) => titleGenerationParser(resTxt),
+    };
+};
+
+const titleGenerationParser = (txt: string) => {
+    const obj: any = {
+        lines: Array<{type: string, explanation: string}>,
+        type: "text"
+    };
+
+    return obj;
+};
