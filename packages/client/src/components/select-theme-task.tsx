5e05e12a0e843ec468b922b44131aa6f4a5c277f
Chase McDougall <chasemcdougall@hotmail.com>
2024-02-07 15:17:07 -0500

    stacked category selections

--- packages/client/src/components/select-theme-task.tsx
+++ packages/client/src/components/select-theme-task.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 
 import { AuthContext } from "../context";
 import { apiGenerateCharacters, apiGenerateSubCategories, apiUpdateTheme } from "../api/theme_api";
@@ -37,6 +37,8 @@ export const SelectThemeTask = (props: Props) => {
     const [selectionStage, setSelectionStage] = useState<number>(0);
     const [loading, setLoading] = useState<boolean>(false);
 
+    const buttonRef = useRef<HTMLDivElement>(null);
+
     const sendLog = () => {
         apiLogEvents(
             context?.token,
@@ -49,10 +51,9 @@ export const SelectThemeTask = (props: Props) => {
             });
     };
 
-    const confirmCategory = () => {
-        setSubCategories([]);
+    const confirmCategory = (value: string) => {
         setLoading(true);
-        apiGenerateSubCategories(context?.token, selectedCategory)
+        apiGenerateSubCategories(context?.token, value)
             .then(async (response) => {
                 const data = await response.json();
                 setSubCategories(data.categories);
@@ -61,13 +62,11 @@ export const SelectThemeTask = (props: Props) => {
             .catch((error) => {
                 logError("confirmCategory: " + error.toString());
             });
-        setSelectionStage(selectionStage + 1);
     };
 
-    const confirmSubCategory = () => {
-        setThemes([]);
+    const confirmSubCategory = (value: string) => {
         setLoading(true);
-        apiGenerateCharacters(context?.token, selectedSubCategory)
+        apiGenerateCharacters(context?.token, value)
             .then(async (response) => {
                 const data = await response.json();
                 setThemes(data.characters);
@@ -76,7 +75,6 @@ export const SelectThemeTask = (props: Props) => {
             .catch((error) => {
                 logError("confirmSubCategory: " + error.toString());
             });
-        setSelectionStage(selectionStage + 1);
     };
 
     const submitTheme = async (): Promise<boolean> => {
@@ -142,62 +140,37 @@ export const SelectThemeTask = (props: Props) => {
         );
     };
 
-    const themeButton = (refText: string, text: string, index: number, setRefText: (text: string) => void) => {
+    const themeButton = (
+        refText: string,
+        text: string,
+        index: number,
+        setRefText: (text: string) => void,
+        gridStage: number
+    ) => {
         return (
             <button
                 key={`button_${index}`}
                 className={(refText === text ? "bg-slate-300 border-black" : "bg-white border-slate-300") + " flex items-center p-4 border rounded-3xl hover:bg-slate-300 w-full h-full"}
-                onClick={() => setRefText(text)}
+                onClick={() => handleTextClick(setRefText, text, gridStage)}
             >
                 <p className="w-full">{text}</p>
             </button>
         );
     };
 
-    const grid = () => {
-        let items: string[] = [];
-        let comparisonValue: string;
-        let setComparisonValue: (text: string) => void;
-
-        let loadingText: string;
-    
-        switch (selectionStage) {
-            case 2:
-                items = themes;
-                comparisonValue = selectedTheme;
-                setComparisonValue = setSelectedTheme;
-                loadingText = "Generating Characters";
-                break;
-            case 1:
-                items = subCategories;
-                comparisonValue = selectedSubCategory;
-                setComparisonValue = setSelectedSubCategory;
-                loadingText = "Generating Sub Categories";
-                break;
-            case 0:
-            default:
-                items = categories;
-                comparisonValue = selectedCategory;
-                setComparisonValue = setSelectedCategory;
-                loadingText = "Generating Categories";
-        }
-
+    const grid = (
+        items: string[],
+        comparisonValue: string,
+        setComparisonValue: (text: string) => void,
+        gridStage: number
+    ) => {
         return (
-            <div className="grid grid-cols-3 auto-rows-fr gap-2 justify-items-center items-center w-full">
-                {!loading ?
-                    items.map((item, index) => {
-                        return (
-                            themeButton(comparisonValue, item, index, setComparisonValue)
-                        );
-                    })
-                :
-                    <div className="col-span-full row-span-full flex flex-row gap-2 bg-white px-6 py-3 rounded-3xl border border-slate-300">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="animate-spin w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-                        </svg>
-                        {loadingText}
-                    </div>
-                }
+            <div key={`grid_${gridStage}`} className="grid grid-cols-3 auto-rows-fr gap-2 justify-items-center items-center w-full">
+                {items.map((item, index) => {
+                    return (
+                        themeButton(comparisonValue, item, index, setComparisonValue, gridStage)
+                    );
+                })}
             </div>
         );
     };
@@ -214,35 +187,88 @@ export const SelectThemeTask = (props: Props) => {
                 disabled = !selectedTheme;
                 break;
             case 1:
-                nextTask = confirmSubCategory;
+                nextTask = handleSubmitTask;
                 nextText = "Generate Sub Categories";
                 disabled = !selectedSubCategory;
                 break;
             case 0:
             default:
-                nextTask = confirmCategory;
+                nextTask = handleSubmitTask;
                 nextText = "Confirm Category";
                 disabled = !selectedCategory;
         }
 
         return (
-            <div className="flex flex-row self-end gap-2">
-                {selectionStage > 0 &&
-                    <button className="bg-sky-200 disabled:opacity-50 disabled:cursor-not-allowed enabled:hover:bg-sky-100 enabled:hover:text-white py-2 px-4 rounded-full" onClick={() => setSelectionStage(selectionStage-1)}>
-                        Back
-                    </button>
-                }
-                <button disabled={disabled} className="bg-sky-200 disabled:opacity-50 disabled:cursor-not-allowed enabled:hover:bg-sky-100 enabled:hover:text-white py-2 px-4 rounded-full" onClick={nextTask}>
-                    {nextText}
-                </button>
+            <div
+                ref={buttonRef}
+                className="flex flex-row self-end gap-2"
+            >
+                {selectionStage >= 2 && <button disabled={disabled} className="bg-sky-200 disabled:opacity-50 disabled:cursor-not-allowed enabled:hover:bg-sky-100 enabled:hover:text-white py-2 px-4 rounded-full" onClick={nextTask}>
+                    Confirm Character
+                </button>}
             </div>
         );
+    };
+
+    const scrollToBottom = () => {
+        buttonRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+    
+    useEffect(() => {
+        scrollToBottom()
+    }, [themes, subCategories, categories]);
+
+    const handleTextClick = (setValue: (text: string) => void, value: string, gridStage: number) => {
+        setValue(value);
+
+        console.log("Grid Stage: " + gridStage);
+    
+        switch (gridStage) {
+            case 0:
+                // Reset sub category and theme
+                setSelectionStage(1);
+                setSelectedTheme("");
+                setSelectedSubCategory("");
+                setThemes([]);
+                setSubCategories([]);
+                confirmCategory(value);
+                break;
+            case 1:
+                // Reset theme
+                setSelectionStage(2);
+                setSelectedTheme("");
+                setThemes([]);
+                confirmSubCategory(value);
+                break;
+            case 2:
+            default:
+                break;
+        }
     };
 
     return (
         <div className="flex flex-col gap-2 py-12 max-w-2xl m-auto">
             {headers()}
-            {grid()}
+            <div className="flex flex-col gap-8 h-full">
+                {[...Array(selectionStage + 1)].map((_e, stage) => {
+                    return(
+                        !loading || selectionStage != stage ?
+                            grid(
+                                stage === 2 ? themes : stage === 1 ? subCategories : categories,
+                                stage === 2 ? selectedTheme : stage === 1 ? selectedSubCategory : selectedCategory,
+                                stage === 2 ? setSelectedTheme : stage === 1 ? setSelectedSubCategory : setSelectedCategory,
+                                stage
+                            )
+                        :
+                            <div key={`grid_loading_${stage}`} className="flex flex-row gap-2 bg-white px-6 py-3 rounded-3xl border border-slate-300 max-w-sm m-auto">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="animate-spin w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                                </svg>
+                                {stage === 2 ? "Generating Characters" : stage === 1 ? "Generating Sub Categories" : "Generating Categories"}
+                            </div>
+                    )
+                })}
+            </div>
             {navButtons()}
         </div>
     );
